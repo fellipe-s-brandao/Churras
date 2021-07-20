b0e95c98f88562a1c32f5ccf24422af10aa7bf28
fellipe-s-brandao <fellipesilverio31@hotmail.com>
2021-07-20 11:04:15 -0300

    css pronto

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Carne - 400gr por pessoa +de 6 horas - 650gr
+// Cerveja - 1200ml por pessoa +de 6 horas 2000ml
+// Refrigerante/agua - 1000ml por pessoa +de 6 horas 1500ml
+
+// Criança valem por 0,5 
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
